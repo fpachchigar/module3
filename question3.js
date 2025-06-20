cf537345f3e16eea9aa380f4b2efc05be2d8b14e
fpachchigar <falgunipancholi1990@gmail.com>
2025-06-21 11:04:54 +1200

    question1.js

---
+++ question3.js
@@ -0,0 +1,8 @@
+//Which of the following console.log messages will print? Why?
+//if (0) console.log("#1 zero is true") 
+//if (null) console.log("null is true")
+
+//below console.log messages will print as o,null are false and not printing the message 
+if (-1)console.log("negative is true")
+if (1) console.log("positive is true")
+if ("0") console.log("#2 zero is true")
