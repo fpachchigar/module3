aee6a264d83740278a03b0379ce982bc19154734
fpachchigar <falgunipancholi1990@gmail.com>
2025-06-21 10:41:15 +1200

    excercise2.js

---
+++ excercise2.js
@@ -0,0 +1,10 @@
+console.log (three = Number("3"));
+console.log (four = Number("4"));
+console.log (thirty = Number("30"))
+//what is the value of the following expressions?
+console.log( addition = three + four); // 7
+console.log(multiplication = three * four); //12
+console.log(division = three / four); //0.75
+console.log(subtraction = three - four); //-1
+console.log(lessThan1 = three < four); //true
+console.log(lessThan2 = thirty < four) //false
