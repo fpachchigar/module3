2654bca31fbaaee4510f2bb269939c3c518e06a6
fpachchigar <falgunipancholi1990@gmail.com>
2025-06-21 14:54:31 +1200

    question8.js

--- question8.js
+++ question8.js
@@ -1,7 +1,44 @@
+function printCitydetails(city) {
+  for (let key in city) {
+    console.log(`${key}: ${city[key]}`);
+  }
+}
 const sydney = {
-    name: 'Sydney',
-    population: 5_121_000,
-    state: 'NSW',
-    founded: '26 January 1788',
-    timezone: 'Australia/Sydney'
-    }
+  name: "Sydney",
+  population: 5_121_000,
+  state: "NSW",
+  founded: "26 January 1788",
+  timezone: "Australia/Sydney",
+};
+
+printCitydetails(sydney);
+
+function printCitydetails(city) {
+  for (let key in city) {
+    console.log(`${key}: ${city[key]}`);
+  }
+}
+const newyork = {
+  name: "New York",
+  population: 8_258_000,
+  state: "New York",
+  founded: "1624",
+  timezone: "NY, USA",
+};
+
+printCitydetails(newyork);
+
+function printCitydetails(city) {
+  for (let key in city) {
+    console.log(`${key}: ${city[key]}`);
+  }
+}
+const auckland = {
+  name: "Auckland",
+  population: 1_530_500,
+  state: "Auckland",
+  founded: "18 September 1840",
+  timezone: "New Zealand/Auckland",
+};
+
+printCitydetails(auckland);
