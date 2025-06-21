3aea03887b10a1ed9be4e08b00526ac258defc15
fpachchigar <falgunipancholi1990@gmail.com>
2025-06-21 16:06:26 +1200

    question10.js

--- question10.js
+++ question10.js
@@ -1,17 +1,41 @@
-/* The following constructor function creates a new Person object with the given name and
-age values.
-a) Create a new person using the constructor function and store it in a variable
-b) Create a second person using different name and age values and store it in a separate
-variable
-c) Print out the properties of each person object to the console
-d) Rewrite the constructor function as a class called PersonClass and use it to create a
-third person using different name and age values. Print it to the console as well.
-e) Add a canDrive method to both the constructor function and the class that returns true
-if the person is old enough to drive.
-function Person(name, age)   */
+//a) Create a new person using the constructor function and store it in a variable
 
-{
-this.name = name;
-this.age = age;
-this.human = true;
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.human = true;
+
+  this.canDrive = function () {
+    return this.age >= 16;
+  };
 }
+//a) first person
+const person1 = new Person("Henry", 25);
+//b) Create a second person using different name and age values and store it in a separate variable
+const person2 = new Person("Helen", 15);
+
+//c) Print out the properties of each person object to the console
+console.log("person 1:", person1);
+console.log("Can Person 1 drive?", person1.canDrive());
+
+console.log("Person 2:", person2);
+console.log("Can Person 2 drive?", person2.canDrive());
+
+/* d) Rewrite the constructor function as a class called PersonClass and use it to create
+ a third person using different name and age values. Print it to the console as well. */
+class PersonClass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.human = true;
+  }
+  //e) Add a canDrive method to both the constructor function and the class that returns true if the person is old enough to drive. function Person(name, age)
+  canDrive() {
+    return this.age >= 16;
+  }
+}
+// third person
+const person3 = new PersonClass("Erin", 19);
+
+console.log("Person 3:", person3);
+console.log("Can Person 3 drive?", person3.canDrive());
